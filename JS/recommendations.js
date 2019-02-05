e96a198f688ec084c454725a623d10ea86d9d8b9
Martin Hwang <resume@martinhwang.ca>
2019-02-05 12:53:18 -0500

    Add README, CSS and HTML

---
+++ JS/recommendations.js
@@ -0,0 +1,18 @@
+function displayDarceeRecommendation()
+{
+    document.getElementById("markMcCaulay").style.display = "none";
+    document.getElementById("sunitaThakur").style.display = "none";
+    document.getElementById("darcee").style.display = "block";
+}
+function displayMarkMcCaulayRecommendation()
+{
+    document.getElementById("sunitaThakur").style.display = "none";
+    document.getElementById("darcee").style.display = "none";
+    document.getElementById("markMcCaulay").style.display = "block";
+}
+function displaySunitaThakurRecommendation()
+{
+    document.getElementById("darcee").style.display = "none";
+    document.getElementById("markMcCaulay").style.display = "none";
+    document.getElementById("sunitaThakur").style.display = "block";
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+Online Resume For Martin Hwang
+It consists of 
+Resume
+Project
+Recommendation Letter
+Co Curricular Record
+
+You can see the project on resume.martinhwang.ca
