a01c4db765ae6400a362512f7f499a2ea983d999
Martin Hwang <resume@martinhwang.ca>
2019-04-22 19:58:02 -0400

    Update resume and letters of recommendation

--- JS/recommendations.js
+++ JS/recommendations.js
@@ -1,21 +1,32 @@
 function displayDarceeRecommendation()
 {
-    document.getElementById("markMcCaulay").style.display = "none";
+    document.getElementById("chelseaWin").style.display = "none";
     document.getElementById("sunitaThakur").style.display = "none";
+    document.getElementById("markMcCaulay").style.display = "none";
     let d = document.getElementById("darcee");
-    d.setAttribute("style", "display: block; margin-left: 10em;")
+    d.setAttribute("style", "display : block; text-align : center");
 }
 function displayMarkMcCaulayRecommendation()
 {
     document.getElementById("sunitaThakur").style.display = "none";
     document.getElementById("darcee").style.display = "none";
+    document.getElementById("chelseaWin").style.display = "none";
     let m = document.getElementById("markMcCaulay");
-    m.setAttribute("style", "display: block; margin-left: 10em;")
+    m.setAttribute("style", "display : block; text-align : center");
 }
 function displaySunitaThakurRecommendation()
 {
     document.getElementById("darcee").style.display = "none";
+    document.getElementById("chelseaWin").style.display = "none";
     document.getElementById("markMcCaulay").style.display = "none";
     let s = document.getElementById("sunitaThakur");
-    s.setAttribute("style", "display: block; margin-left: 10em;");
+    s.setAttribute("style", "display : block; text-align : center");
+}
+function displayChelseaWinRecommendation()
+{
+    document.getElementById("darcee").style.display = "none";
+    document.getElementById("markMcCaulay").style.display = "none";
+    document.getElementById("sunitaThakur").style.display = "none";
+    let c = document.getElementById("chelseaWin");
+    c.setAttribute("style", "display : block; text-align : center");
 }
